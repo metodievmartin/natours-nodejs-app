d72e3ee58ae7001320f1f91d653ea9be2281bdb9
Martin Metodiev <martin.metodiew@gmail.com>
2021-05-16 16:57:13 +0300

    Add error handling middleware

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require("./utils/AppError");
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
@@ -14,5 +17,11 @@ if (process.env.NODE_ENV === 'development') {
 // Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
