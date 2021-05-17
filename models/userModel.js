9bea115e4b4da63012500a6be1462ba20722c3ea
Martin Metodiev <martin.metodiew@gmail.com>
2021-05-17 13:28:04 +0300

    Create user schema model

---
+++ models/userModel.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please tell us your name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide your email'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email']
+    },
+    photo: String,
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: [8, 'Your password cannot be less than 8 characters']
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password'],
+        validate: {
+            // This only works on SAVE and CREATE
+            validator: function (currentVal) {
+                return currentVal === this.password;
+            },
+            message: 'Password don\'t match'
+        }
+    }
+});
+
+userSchema.pre('save', async function (next) {
+    // Runs only when the password has been modified
+    if (!this.isModified('password')) return next();
+
+    // Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+    // Clear the passwordConfirm field
+    this.passwordConfirm = undefined;
+
+    next();
+});
+
+module.exports = mongoose.model('User', userSchema);
