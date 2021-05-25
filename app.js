86e263b0d2ab934fab9014f37fd7ecc42ac6b73f
Martin Metodiev <martin.metodiew@gmail.com>
2021-05-25 13:46:29 +0300

    Add request rate limit from a single IP address

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
 const globalErrorHandler = require('./controllers/errorController');
@@ -7,12 +9,20 @@ const AppError = require("./utils/AppError");
 
 const app = express();
 
-// Middleware
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+// GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many request from this IP address. Please try again in an hour!'
+});
+
+app.use('/api', limiter);
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 // Routes
 app.use('/api/v1/tours', tourRouter);
