e5ea9ec423a3689b268be18e157ee7d6ddb0456b
Martin Metodiev <martin.metodiew@gmail.com>
2021-05-29 00:19:23 +0300

    Create a factory function handler for updating one document

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -16,3 +16,26 @@ exports.deleteOne = Model =>
             data: null
         });
     });
+
+// Factory function that returns a generic handler to update a single document
+exports.updateOne = Model =>
+    catchAsync(async (req, res, next) => {
+        const docID = req.params.id;
+
+        const doc = await Model.findByIdAndUpdate(docID, req.body, {
+            new: true,
+            runValidators: true,
+            useFindAndModify: false
+        });
+
+        if (!doc) {
+            return next(new AppError('No document found with this ID', 404));
+        }
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                data: doc
+            }
+        });
+    });
