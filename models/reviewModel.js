03c01c0a5b5e731b211ba6407bd26d5da33ea35b
Martin Metodiev <martin.metodiew@gmail.com>
2021-06-08 20:59:31 +0300

    Add functionality to dynamically calculate the avg rating & review count on each tour when a review is created/updated/deleted

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const Tour = require('./tourModel');
+
 const reviewSchema = new mongoose.Schema({
     review: {
         type: String,
@@ -24,18 +26,65 @@ const reviewSchema = new mongoose.Schema({
         ref: 'User',
         required: [true, 'Review must belong to a user']
     }
-},{
+}, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
 });
 
+// -- STATIC METHODS --
+
+// Calculates the average rating & the reviews count for a provided tour and updates in the DB
+reviewSchema.statics.calcAverageRating = async function(tourId) {
+    // 'this' refers to the current model
+    const stats = await this.aggregate([
+        {
+            $match: { tour: tourId }
+        },
+        {
+            $group: {
+                _id: '$tour',
+                nRating: { $sum: 1 },
+                avgRating: { $avg: '$rating' }
+            }
+        }
+    ]);
+
+    // Update the tour with the new values, in case there are no reviews set default values
+    if (stats.length > 0) {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: stats[0].nRating,
+            ratingsAverage: stats[0].avgRating
+        }, {
+            useFindAndModify: false
+        });
+    } else {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: 0,
+            ratingsAverage: 4.5
+        },{
+            useFindAndModify: false
+        });
+    }
+};
+
+// -- DOCUMENT MIDDLEWARE --
+
+// Runs each time a new review is created in order to update the parent tour with the new values
+reviewSchema.post('save', async function () {
+    // 'this' refers to the current review
+
+    // Call the static method on the review model to calculate the new avg rating & review count & save it to the tour
+    await this.constructor.calcAverageRating(this.tour);
+    // Review.calcAverageRating(this.tour);
+});
+
 // -- QUERY MIDDLEWARE --
-// Using regex to match all the methods starting with 'find' (findOne(), findByID(), etc.)
+// Uses regex to match all the methods starting with 'find' (findOne(), findByID(), etc.)
 // otherwise it will run only for the .find() method
 
-// Populate the 'tour' & 'user' fields with the selected data on each request,
+// Populates the 'tour' & 'user' fields with the selected data on each request,
 // querying by the stored 'tour' & 'user' id references
-reviewSchema.pre(/^find/, function(next) {
+reviewSchema.pre(/^find/, function (next) {
     /*
     this.populate({
             path: 'tour',
@@ -52,6 +101,24 @@ reviewSchema.pre(/^find/, function(next) {
     });
 
     next()
+});
+
+// Decorates the 'this' object for the 'post' middleware
+reviewSchema.pre(/^findOneAnd/, async function(next) {
+    // Since we don't have access to the current review document in the query middleware
+    // we fetch it and attach it to the 'this' obj so that we can access it from the 'post' middleware func
+   this.r = await this.findOne();
+
+   next();
+});
+
+// Runs on each findOneAndUpdate() or findOneAndDelete() called on the review
+// in order to update the parent tour with the new values
+reviewSchema.post(/^findOneAnd/, async function() {
+    // await this.findOne(); does NOT work here, query has already executed - using the previously attached 'r' obj
+
+    // Call the static method on the review model to calculate the new avg rating & review count & save it to the tour
+    await this.r.constructor.calcAverageRating(this.r.tour);
 });
 
 module.exports = mongoose.model('Review', reviewSchema);
