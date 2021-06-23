9d7b6f149fe978b772312458ca3aedf1b5f3cee6
Martin Metodiev <martin.metodiew@gmail.com>
2021-06-23 16:23:16 +0300

    Create the tour page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -12,9 +12,17 @@ exports.getOverview = catchAsync(async (req, res) => {
     })
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+    // Fetch the tour & populate the 'review'
+    const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+        path: 'reviews',
+        fields: 'review rating user'
+    });
+
+    // Render the page with tour data
     res.status(200).render('tour', {
-        title: 'The Forest Hiker'
+        title: 'The Forest Hiker',
+        tour
     })
-};
+});
 
