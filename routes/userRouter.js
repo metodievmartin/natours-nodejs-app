c8f299f2831e6c1dec9ea359a967eb787c7bf89b
Martin Metodiev <martin.metodiew@gmail.com>
2021-07-05 22:36:19 +0300

    Refactor code

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,58 +1,40 @@
 const express = require("express");
-const {
-    signup,
-    login,
-    forgotPassword,
-    resetPassword,
-    protect,
-    restrictTo,
-    updatePassword,
-    logout
-} = require("../controllers/authController");
-const {
-    createUser,
-    getAllUsers,
-    deleteUser,
-    getUser,
-    updateUser,
-    getMe,
-    updateMe,
-    deleteMe
-} = require("../controllers/userController");
+const authCtrl = require("../controllers/authController");
+const userCtrl = require("../controllers/userController");
 
 // '/api/v1/users'
 const router = express.Router();
 
 // Authorization actions endpoints - access by everyone
-router.post('/signup', signup);
-router.post('/login', login);
-router.get('/logout', logout);
+router.post('/signup', authCtrl.signup);
+router.post('/login', authCtrl.login);
+router.get('/logout', authCtrl.logout);
 
 // Password actions endpoints - access by everyone
-router.post('/forgotPassword', forgotPassword);
-router.patch('/resetPassword/:token', resetPassword);
+router.post('/forgotPassword', authCtrl.forgotPassword);
+router.patch('/resetPassword/:token', authCtrl.resetPassword);
 // - access by logged users
-router.patch( '/updateMyPassword', protect, updatePassword);
+router.patch( '/updateMyPassword', authCtrl.protect, authCtrl.updatePassword);
 
 // Use the protect middleware here to auth guard all of the routes below
-router.use(protect);
+router.use(authCtrl.protect);
 
 // Current user actions endpoints - access by logged users
-router.get('/me', getMe, getUser);
-router.patch( '/updateMe', updateMe);
-router.delete( '/deleteMe', deleteMe);
+router.get('/me', userCtrl.getMe, userCtrl.getUser);
+router.patch( '/updateMe', userCtrl.uploadUserPhoto, userCtrl.updateMe);
+router.delete( '/deleteMe', userCtrl.deleteMe);
 
 // Use middleware here to restrict the access by role to all of the routes below
-router.use(restrictTo('admin'));
+router.use(authCtrl.restrictTo('admin'));
 
 // Users CRUD actions - access by logged admin users only
 router.route('/')
-    .get(getAllUsers)
-    .post(createUser);
+    .get(userCtrl.getAllUsers)
+    .post(userCtrl.createUser);
 
 router.route('/:id')
-    .get(getUser)
-    .patch(updateUser)
-    .delete(deleteUser);
+    .get(userCtrl.getUser)
+    .patch(userCtrl.updateUser)
+    .delete(userCtrl.deleteUser);
 
 module.exports = router;
