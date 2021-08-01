288bfaa95c53bfb66c5b0555c69754638cc6ffa5
Martin Metodiev <martin.metodiew@gmail.com>
2021-08-01 17:31:19 +0300

    feat: create automated email handler
    Create an Email class with newTransport() & send() methods that set up everything necessary for sending.
    This way new automated emails can easily be created by adding a new method for each one and calling send()
    with a template's name & email's subject as parameters

--- utils/email.js
+++ utils/email.js
@@ -1,26 +1,58 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async options => {
-    // 1) Create transporter
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD
+module.exports = class Email {
+    constructor(user, url) {
+        this.to = user.email;
+        this.firstName = user.name.split(' ')[0];
+        this.url = url;
+        this.from = `Martin Wigans <${process.env.EMAIL_FROM}>`
+    }
+
+    newTransport() {
+        if (process.env.NODE_ENV === 'production') {
+            // Sendgrid
+            // TODO: fix the account and verify domain sender on https://app.sendgrid.com/
+            return nodemailer.createTransport({
+                service: 'SendGrid',
+                auth: {
+                    user: process.env.SENDGRID_USERNAME,
+                    pass: process.env.SENDGRID_PASSWORD,
+                }
+            });
         }
-    });
 
-    // 2) Define the email options
-    const mailOptions = {
-        from: 'Martin Wigans <wigans@example.com>',
-        to: options.email,
-        subject: options.subject,
-        text: options.message
-    };
+        return nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD
+            }
+        });
+    }
 
-    // 3) Actually send the email
-    await transporter.sendMail(mailOptions);
-};
+    // Send the actual email
+    async send(template, subject) {
+        // 1) Render HTML based on a pug template
+        const html = pug.renderFile(`${__dirname}/../views/emails/${template}.pug`, {
+            firstName: this.firstName,
+            url: this.url,
+            subject
+        });
 
-module.exports = sendEmail;
+        // 2) Define email options
+        const mailOptions = {
+            from: this.from,
+            to: this.to,
+            subject,
+            html,
+            text: htmlToText.fromString(html)
+        };
+
+        // 3) Create a transport and send email
+        await this.newTransport().sendMail(mailOptions);
+    }
+}
+
